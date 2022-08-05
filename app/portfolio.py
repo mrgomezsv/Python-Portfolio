5369de28f5bddc43a4f6e1eabd5ab740ef5ff402
Mario Roberto Gómez <mrgomez.dev@gmail.com>
2022-08-05 05:26:14 -0600

    delet directory venv and new create directory venv for error S.O. RUN OK

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,6 +1,8 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, redirect, render_template, request, redirect, url_for, current_app
 )
+import sendgrid
+from sendgrid.helpers.mail import *
 
 bp = Blueprint('portfolio', __name__, url_prefix='/')
 
@@ -8,6 +10,37 @@ bp = Blueprint('portfolio', __name__, url_prefix='/')
 def index():
     return render_template('portfolio/index.html')
 
-@bp.route('/mail', methods=['POST'])
+@bp.route('/mail', methods=['GET', 'POST'])
 def mail():
-    return render_template('portfolio/sent_mail.html')
+    name = request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    message = request.form.get('message')
+
+    if request.method == 'POST':
+        send_mail(name, email, phone, message)
+        return render_template('portfolio/sent_mail.html')
+
+    return redirect(url_for('portfolio.index'))
+
+def send_mail(name, email, phone, message):
+    mi_email = 'mrgomez.dev@gmail.com'
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
+
+    from_email = Email(mi_email)
+    to_email = To(mi_email, substitutions={
+        "-name-" : name,
+        "-email-" : email,
+        "-phone-" : phone,
+        "-message-" : message,
+    })
+
+    html_content = """
+        <p>Hola Mario, tienes un nuevo contacto desde la web:</p>
+        <p>Nombre: -name-</p>
+        <p>Correo: -mail-</p>
+        <p>Telefono: -phone-</p>
+        <p>Mensaje: -message-</p>
+    """
+    mail = Mail(mi_email, to_email, "Nuevo contacto desde la web", html_content=html_content)
+    response = sg.client.mail.send.post(request_body=mail.get())
